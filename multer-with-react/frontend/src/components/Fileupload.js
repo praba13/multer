1eb5b62e418548f5f2e793a7827eeb67da79f698
Praba <prama1303@gmail.com>
2023-03-12 01:25:49 +0100

    upload from frontend to backend project finished

--- multer-with-react/frontend/src/components/Fileupload.js
+++ multer-with-react/frontend/src/components/Fileupload.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 const Fileupload = () => {
   const [file, setFile] = useState();
   const [fileName, setFileName] = useState('');
+  const [resultText, setResultText] = useState('');
 
   const fileInput = useRef();
 
@@ -23,9 +24,22 @@ const Fileupload = () => {
     formData.append('fileName', fileName);
 
     try {
-      await axios.post('http://localhost:3001/upload', formData);
+      const res = await axios.post('http://localhost:3001/upload', formData);
+      //console.log(res);
+      setResultText(res.data.message);
+      fileInput.current.value = '';
+      setTimeout(() => {
+        setResultText('');
+      }, 5000);
     } catch (error) {
-      console.log(error);
+      if (error.response !== undefined) {
+        setResultText(error.response.data.message);
+      } else {
+        setResultText('Server Error!');
+      }
+      setTimeout(() => {
+        setResultText('');
+      }, 5000);
     }
   };
 
@@ -35,6 +49,7 @@ const Fileupload = () => {
       <button className='btn btn-success' onClick={uploadFile}>
         Upload
       </button>
+      {resultText ? <p>{resultText}</p> : null}
     </div>
   );
 };
